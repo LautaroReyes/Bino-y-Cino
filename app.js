d990ec56b5e9563d7ac702efff965ff24e1425ec
LautaroReyes <129559110+LautaroReyes@users.noreply.github.com>
2023-04-14 15:18:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
-let numero = prompt("Ingrese un numero");
-numero = numero.split(" ");
-let n = [];
-let n1;
-for (let i = 0; i < numero.length ; i++) {
-       n[i] = parseInt(numero[i]);
-       n1 = Math.pow(n[i],2);
-       console.log(n1);
+let n1 = prompt("Ingrese numero");
+let contadorMult2 = 0;
+let contadorMult3 = 0;
+let contadorMult4 = 0;
+let contadorMult5 = 0;
+
+
+console.log(n1);
+
+n1 = n1.split(" ");
+let n2 = [];
+for (let i = 0; i < n1.length; i++) {
+    n2[i] = parseInt(n1[i]);    
+    if(n2[i]%2==0) {
+        ContarMultiplosde2();
+    }else if(n2[i]%3==0){
+        ContarMultiplosde3();
+    }else if(n2[i]%4==0){
+        ContarMultiplosde4();
+
+    }else if(n2[i]%5==0){
+        ContarMultiplosde5();
+    }
+    
+} 
+document.write("Multiplos de 2: " + contadorMult2 + "<br>");
+document.write("Multiplos de 3: " + contadorMult3 + "<br>");
+document.write("Multiplos de 4: " + contadorMult4 + "<br>");
+document.write("Multiplos de 5: " + contadorMult5 + "<br>");
+
+
+function ContarMultiplosde2(){
+    contadorMult2++; //Suma uno al contador
+}
+function ContarMultiplosde3(){
+    contadorMult3++;
+}
+function ContarMultiplosde4(){
+    contadorMult4++;
+}
+function ContarMultiplosde5(){
+    contadorMult5++;
 }
